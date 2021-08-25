88d54049151218bffd153bba872c4bc90013a8b4
朱汶轩 <wenxuan.zhu@70capital.com>
2021-08-25 09:52:33 +0800

    add change to pareto factor

--- pareto.py
+++ pareto.py
@@ -1,5 +1,6 @@
 import sys
 import cudf
+
 sys.path.append('/mnt/lustre/app/qllib')
 
 from hft_signal_maker.hft_pipeline import HftPipeline
@@ -24,7 +25,8 @@ def get_pareto(cxt):
         columns={'bid_id': 'order_id', 'real_bid_volume': 'volume'}),
                          trander[['code', 'ask_id', 'real_ask_volume']].rename(
                              columns={'ask_id': 'order_id', 'real_ask_volume': 'volume'})])
-    order = order[order.volume != 0].drop_duplicates()
+    order = order.sort_values(['code', 'order_id', 'volume'])
+    order = order[order.volume != 0].drop_duplicates(subset=['code', 'order_id'], keep='last')
     pareto = order.groupby('code').volume.quantile(0.25) / order.groupby('code').volume.quantile(0.75)
     res = pareto.reset_index().rename(columns={'volume': 'pareto'})
     res['time_flag'] = 150000
